6781876364b98350d80425c7bda0ab68259436a0
odinellefsen <49004766+odinellefsen@users.noreply.github.com>
2024-02-15 22:22:08 +0000

    feat: added pwb

---
+++ next.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import("next").NextConfig} */
+
+const withPWA = require("@ducanh2912/next-pwa").default({
+    dest: "public",
+    cacheOnFrontEndNav: true,
+    aggressiveFrontEndNavCaching: true,
+    reloadOnOnline: true,
+    swcMinify: true,
+    // disable: false,
+    disable: process.env.NODE_ENV === "development",
+    workboxOptions: {
+        disableDevLogs: true
+    }
+})
+
+const nextConfig = {};
+
+module.exports = withPWA(nextConfig);
