84b75e6dc3049cdbe6388a493ef6f4d383f32471
René Bischoff <rene.bischoff@gmail.com>
2024-02-20 09:54:11 +0000

    chore: update sendWebhook function to use new webhook URL structure
    - Updated the `sendWebhook` function to use a new URL structure for the webhook endpoint.
    - The new URL structure includes the `FLOWCORE_WEBHOOK_DOMAIN`, `FLOWCORE_TENANT`, and `FLOWCORE_DATACORE` environment variables.
    - This change ensures that the webhook is sent to the correct endpoint based on the environment configuration.

--- src/lib/webhook.ts
+++ src/lib/webhook.ts
@@ -1,7 +1,18 @@
-import axios from "axios" 
-import * as process from "process";
+import axios from "axios"
+import * as process from "process"
 
-export async function sendWebhook<T>(aggregator: string, event: string, data: T) {
-  const url = [process.env.FLOWCORE_DATACORE!, aggregator, event].join("/")
-  await axios.post(url, data, {params: {key: process.env.FLOWCORE_KEY}})
+export async function sendWebhook<T>(
+  aggregator: string,
+  event: string,
+  data: T,
+) {
+  const url = [
+    process.env.FLOWCORE_WEBHOOK_DOMAIN,
+    "event",
+    process.env.FLOWCORE_TENANT,
+    process.env.FLOWCORE_DATACORE,
+    aggregator,
+    event,
+  ].join("/")
+  await axios.post(url, data, { params: { key: process.env.FLOWCORE_KEY } })
 }
