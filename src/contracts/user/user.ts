3f03f1876bc3a0e940ec413988ddfece1e260817
flowcore-platform <gitops@flowcore.io>
2024-02-27 12:20:26 +0000

    feat: user added to context, with dynamic user creation

---
+++ src/contracts/user/user.ts
@@ -0,0 +1,9 @@
+import {z} from "zod";
+import {UserRole} from "@/contracts/user/user-role";
+
+const UserDto = z.object({
+  id: z.string(),
+  role: z.nativeEnum(UserRole)
+})
+
+export type User = z.infer<typeof UserDto>;
