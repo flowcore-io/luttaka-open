58c832df7ec5eb5ec15d356f8ecf6d1939a51089
flowcore-platform <gitops@flowcore.io>
2024-02-29 11:49:52 +0000

    refactor: renamed file to avoid name conflicts

--- src/server/api/routers/user/user.router.ts
+++ src/server/api/routers/user/user.router.ts
@@ -1,6 +1,6 @@
 import { createTRPCRouter } from "@/server/api/trpc"
 import { getUserRouter } from "@/server/api/routers/user/user-get"
-import { getUserRoleRouter } from "@/server/api/routers/user/user-role"
+import { getUserRoleRouter } from "@/server/api/routers/user/user-get-role"
 import { setUserRoleRouter } from "@/server/api/routers/user/user-set-role"
 
 export const userRouter = createTRPCRouter({
