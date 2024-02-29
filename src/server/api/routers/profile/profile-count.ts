a45657a7d00264ebb0085a0f7732f1dba0fc7b68
flowcore-platform <gitops@flowcore.io>
2024-02-29 12:08:28 +0000

    refactor: merged services into procedures

--- src/server/api/routers/profile/profile-count.ts
+++ src/server/api/routers/profile/profile-count.ts
@@ -1,8 +1,14 @@
 import { protectedProcedure } from "@/server/api/trpc"
-import { getTotalNumberOfProfiles } from "@/server/api/services/profile/page-profiles"
+import { db } from "@/database"
+import { count } from "drizzle-orm"
+import { profiles } from "@/database/schemas"
 
 export const profileCountRouter = protectedProcedure.query(
   async (): Promise<number> => {
-    return getTotalNumberOfProfiles()
+    // todo: convert this to only fetch the number of profiles associated a relevant conference
+    return db
+      .select({ value: count() })
+      .from(profiles)
+      .then((result) => result[0]?.value ?? 0)
   },
 )
--- src/server/api/routers/profile/profile-page-profiles.ts
+++ src/server/api/routers/profile/profile-page-profiles.ts
@@ -1,10 +1,48 @@
 import { protectedProcedure } from "@/server/api/trpc"
 import { PaginationInput } from "@/contracts/pagination/pagination"
 import type { PagedProfileResult } from "@/contracts/profile/paged-profiles"
-import { pageProfiles } from "@/server/api/services/profile/page-profiles"
+import { db } from "@/database"
+import type { UserProfile } from "@/contracts/profile/user-profile"
+import { getInitialsFromString } from "@/server/lib/format/get-initials-from-string"
 
 export const pageProfilesRouter = protectedProcedure
   .input(PaginationInput)
   .query(async ({ input }): Promise<PagedProfileResult> => {
-    return pageProfiles(input)
+    const safePage = Math.max(input.page - 1, 0)
+
+    const profiles = await db.query.profiles.findMany({
+      offset: safePage * input.pageSize,
+      limit: input.pageSize,
+      orderBy: (profile, { asc }) => [asc(profile.firstName)],
+    })
+
+    if (!profiles.length) {
+      return {
+        items: [],
+        page: 0,
+        pageSize: 0,
+      }
+    }
+
+    return {
+      page: input.page,
+      pageSize: profiles.length,
+      items: profiles.map((profile): UserProfile => {
+        const displayName = `${profile.firstName} ${profile.lastName}`
+        const initials = getInitialsFromString(displayName)
+        return {
+          id: profile.id,
+          userId: profile.userId,
+          displayName: displayName,
+          firstName: profile.firstName ?? "",
+          lastName: profile.lastName ?? "",
+          title: profile.title ?? "",
+          description: profile.description ?? "",
+          socials: profile.socials ?? "",
+          company: profile.company ?? "",
+          avatarUrl: profile.avatarUrl ?? "",
+          initials,
+        }
+      }),
+    }
   })
