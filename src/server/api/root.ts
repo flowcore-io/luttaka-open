0c66e6bce03292afa4d256fd4288081d8b9e9c98
flowcore-platform <gitops@flowcore.io>
2024-02-29 11:50:40 +0000

    refactor: moved and renamed profile router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,5 @@
 import { conferenceRouter } from "@/server/api/routers/conference/conference.router"
-import { profileRouter } from "@/server/api/routers/profile"
+import { profileRouter } from "@/server/api/routers/profile/profile.router"
 import ticketRouter from "@/server/api/routers/ticket/ticket.router"
 import { userRouter } from "@/server/api/routers/user/user.router"
 import { createTRPCRouter } from "@/server/api/trpc"
