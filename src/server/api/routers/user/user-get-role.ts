8c89164a5a0ac814cc3eca328200ab0205819055
flowcore-platform <gitops@flowcore.io>
2024-03-01 15:27:04 +0000

    refactor: removed conflicting files

--- src/server/api/routers/user/user-get-role.ts
+++
@@ -1,8 +0,0 @@
-import { UserRole } from "@/contracts/user/user-role"
-import { protectedProcedure } from "@/server/api/trpc"
-
-export const getUserRoleRouter = protectedProcedure.query(
-  async ({ ctx }): Promise<UserRole> => {
-    return (ctx.user.role as UserRole) ?? UserRole.user
-  },
-)
