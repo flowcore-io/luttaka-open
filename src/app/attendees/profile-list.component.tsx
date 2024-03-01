857ec3dd16d634b5c2571a81ed2b8ed13075c8f6
flowcore-platform <gitops@flowcore.io>
2024-03-01 10:01:45 +0000

    refactor: moved the list item in the profile list into a separate component

--- src/app/attendees/profile-list.component.tsx
+++ src/app/attendees/profile-list.component.tsx
@@ -1,7 +1,6 @@
-import Link from "next/link"
 import React, { type FC } from "react"
 
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { ProfileListItem } from "@/app/attendees/profile-list-item.component"
 import { MissingText } from "@/components/ui/messages/missing-text"
 import { type UserProfile } from "@/contracts/profile/user-profile"
 
@@ -17,29 +16,7 @@ export const ProfileList: FC<ProfileListProps> = ({ profiles }) => {
   return (
     <ul className={"space-y-2"}>
       {profiles.map((profile) => {
-        return (
-          <Link href={`/profiles/${profile.id}`}>
-            <li
-              key={profile.id}
-              className={
-                "flex items-center space-x-3 border-b border-accent py-3"
-              }>
-              <Avatar className={"h-12 w-12 flex-grow-0 rounded-full"}>
-                <AvatarImage
-                  src={profile.avatarUrl}
-                  alt={profile.displayName}
-                />
-                <AvatarFallback className={"rounded"}>
-                  {profile.initials}
-                </AvatarFallback>
-              </Avatar>
-              <div className={"flex-1"}>
-                <p className={"font-bold"}>{profile.displayName}</p>
-                <p className={"font-thin"}>{profile.title || "No Title"}</p>
-              </div>
-            </li>
-          </Link>
-        )
+        return <ProfileListItem profile={profile} />
       })}
     </ul>
   )
