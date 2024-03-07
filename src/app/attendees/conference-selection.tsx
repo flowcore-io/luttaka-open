7b8abf64bf9db20f02b590af08c7c9020aa68091
flowcore-platform <gitops@flowcore.io>
2024-03-07 14:35:54 +0000

    refactor: added ref default prevention, to get around an issue with radix

--- src/app/attendees/conference-selection.tsx
+++ src/app/attendees/conference-selection.tsx
@@ -8,6 +8,7 @@ import {
   SelectValue,
 } from "@/components/ui/select"
 import { type ConferencePreview } from "@/contracts/conference/conference"
+import { preventDefaultOnReference } from "@/lib/events/prevent-default-on-reference"
 
 export type ConferenceSelectionProps = {
   conferences: ConferencePreview[]
@@ -20,7 +21,7 @@ export const ConferenceSelection: FC<ConferenceSelectionProps> = (props) => {
       <SelectTrigger>
         <SelectValue placeholder={"Conferences you have tickets for"} />
       </SelectTrigger>
-      <SelectContent>
+      <SelectContent ref={preventDefaultOnReference}>
         {props.conferences.map((conference) => (
           <SelectItem key={conference.id} value={conference.id}>
             {conference.name}
