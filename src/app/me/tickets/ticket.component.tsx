5e8ba56276fc099dcdfe97afe9221611dc2007c5
René Bischoff <rene.bischoff@gmail.com>
2024-03-07 12:33:41 +0100

    fix: fix layout

--- src/app/me/tickets/ticket.component.tsx
+++ src/app/me/tickets/ticket.component.tsx
@@ -88,12 +88,10 @@ export function Ticket({ ticket, refetch, selected, onSelect }: TicketProps) {
   return (
     <>
       <div className="group relative mb-2 flex h-28 space-x-4 rounded-lg border p-2 shadow transition hover:shadow-lg">
-        {ticket.state === "open" && (
-          <div
-            className={`${!selected && "invisible"} absolute left-2 top-2 group-hover:visible`}>
-            <Checkbox checked={selected} onClick={() => onSelect(!selected)} />
-          </div>
-        )}
+        <div
+          className={`${!selected && "invisible"} absolute left-2 top-2 ${ticket.state === "open" ? "group-hover:visible" : ""}`}>
+          <Checkbox checked={selected} onClick={() => onSelect(!selected)} />
+        </div>
         <div className="hidden w-32 items-center justify-center self-stretch sm:flex">
           <Image
             alt={"Tonik"}
