6d2bc4e154b8c8fe544ae8efb771f3e83d4bbb11
olavurellefsen <olavur@ellefsen.fo>
2024-03-24 09:31:18 +0000

    Suppress hydration warning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning={true}>
       <body className={`min-h-screen ${inter.variable} font-inter`}>
         <ThemeProvider
           attribute={"class"}
