404b2c6460fed91fbb1cb90230d4da8cd0144501
olavurellefsen <olavur@ellefsen.fo>
2024-03-25 15:33:20 +0000

    Rename link from attendees to networking

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -33,8 +33,8 @@ const Sidebar = () => {
             icon: Ticket,
           },
           {
-            href: "/attendees",
-            title: "Attendees",
+            href: "/networking",
+            title: "Networking",
             label: "",
             superuserRequired: false,
             icon: Users,
