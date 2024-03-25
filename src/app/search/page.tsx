802f64efe4d116236ea702331ebaa02034ce8688
olavurellefsen <olavur@ellefsen.fo>
2024-03-25 15:32:40 +0000

    Add dummy program and search pages

---
+++ src/app/search/page.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export default function SearchPage() {
+  return (
+    <main className="mx-auto w-full">
+      <div className="mb-6 text-3xl font-bold">Search</div>
+    </main>
+  )
+}
