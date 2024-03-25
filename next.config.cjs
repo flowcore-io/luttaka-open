cf64344fa31f36b5d95ef1ff1ed55512798b015b
olavurellefsen <olavur@ellefsen.fo>
2024-03-25 15:29:25 +0000

    Attempt at making the next/image component work with Clerk

--- next.config.cjs
+++ next.config.cjs
@@ -1,20 +1,31 @@
 /** @type {import("next").NextConfig} */
 
 const withPWA = require("@ducanh2912/next-pwa").default({
-    dest: "public",
-    cacheOnFrontEndNav: true,
-    aggressiveFrontEndNavCaching: true,
-    reloadOnOnline: true,
-    swcMinify: true,
-    // disable: false,
-    disable: process.env.NODE_ENV === "development",
-    workboxOptions: {
-        disableDevLogs: true
-    }
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  // disable: false,
+  disable: process.env.NODE_ENV === "development",
+  workboxOptions: {
+    disableDevLogs: true,
+  },
 })
 
-const removeImports = require("next-remove-imports")();
+const removeImports = require("next-remove-imports")()
 
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+        port: "",
+        path: "**",
+      },
+    ],
+  },
+}
 
-module.exports = withPWA(removeImports(nextConfig));
+module.exports = withPWA(removeImports(nextConfig))
