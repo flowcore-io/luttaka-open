f91d3c0238134431bcf726d78d04862000e6a962
olavurellefsen <olavur@ellefsen.fo>
2024-03-29 20:53:59 +0000

    chore: remove admin banner and add missing padding

--- src/app/protected-page.tsx
+++ src/app/protected-page.tsx
@@ -1,22 +1,13 @@
 "use client"
 
 import Header from "@/components/header"
-import { RestrictedToRole } from "@/components/restricted-to-role"
 import Sidebar from "@/components/sidebar"
-import { UserRole } from "@/contracts/user/user-role"
 import { BottomBar } from "../components/bottom-bar"
 import { ConferenceProvider } from "@/context/conference-context"
 
 const ProtectedPage = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-screen">
-      <RestrictedToRole role={UserRole.admin}>
-        <div className={"bg-primary text-center"}>
-          <p className={"text-[0.85rem] font-thin text-primary-foreground"}>
-            you are an <b>admin</b>
-          </p>
-        </div>
-      </RestrictedToRole>
+    <div className="">
       <ConferenceProvider>
         <div className="flex flex-row">
           <div className="hidden sm:block">
@@ -25,7 +16,7 @@ const ProtectedPage = ({ children }: { children: React.ReactNode }) => {
           <div className="relative flex w-96 flex-1 flex-col">
             <Header />
             <main>
-              <div className="mx-auto mb-24 max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+              <div className="mx-auto mb-24 max-w-screen-2xl 2xl:p-10">
                 {children}
               </div>
             </main>
