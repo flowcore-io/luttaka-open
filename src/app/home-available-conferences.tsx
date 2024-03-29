dcdf535046300e3ba772936a9dbc672318184e5a
olavurellefsen <olavur@ellefsen.fo>
2024-03-29 20:39:51 +0000

    feat: component with available conferences if none has been chosen

---
+++ src/app/home-available-conferences.tsx
@@ -0,0 +1,36 @@
+import { Skeleton } from "@/components/ui/skeleton"
+import { type appRouter } from "@/server/api/root"
+import { inferRouterOutputs } from "@trpc/server"
+import { api } from "@/trpc/react"
+import { PageTitle } from "@/components/ui/page-title"
+import { ConferencesList } from "../components/conferences-list"
+
+type RouterOutput = inferRouterOutputs<typeof appRouter>
+
+interface HomeAvailableConferencesProps {
+  conferences: RouterOutput["conference"]["list"]
+  isLoading: boolean
+}
+
+export default function HomeAvailableConferences({
+  conferences,
+  isLoading,
+}: HomeAvailableConferencesProps) {
+  const apiFetchAttendingConferences = api.attendance.myConferences.useQuery()
+
+  return (
+    <div className="mx-auto w-full p-4 md:p-6">
+      <PageTitle
+        title={"Your events"}
+        subtitle={"A list of events that you have tickets to"}
+      />
+      {isLoading || !conferences ? (
+        <Skeleton />
+      ) : (
+        <ConferencesList
+          conferences={apiFetchAttendingConferences.data ?? []}
+        />
+      )}
+    </div>
+  )
+}
---
+++ src/components/conferences-list.tsx
@@ -0,0 +1,46 @@
+import { Button } from "@/components/ui/button"
+import { ConferenceContext } from "@/context/conference-context"
+import dayjs from "dayjs"
+import { useContext } from "react"
+import { type ConferenceProfile } from "@/contracts/conference/conference"
+
+interface ConferencesListProps {
+  conferences: ConferenceProfile[]
+}
+export function ConferencesList({ conferences }: ConferencesListProps) {
+  return conferences
+    .sort((a, b) => {
+      return dayjs(a.startDate).isBefore(dayjs(b.startDate)) ? -1 : 1
+    })
+    .map((conference) => (
+      <Conference conference={conference} key={conference.id} />
+    ))
+}
+interface ConferenceProps {
+  conference: ConferenceProfile
+}
+function Conference({ conference }: ConferenceProps) {
+  const { conferenceId, setConferenceId, setConferenceName } =
+    useContext(ConferenceContext)
+  return (
+    <div
+      key={conference.id}
+      className={`mb-6 p-2 ${conference.id === conferenceId ? "bg-slate-200" : ""}`}>
+      {conference.id !== conferenceId && (
+        <Button
+          onClick={() => {
+            setConferenceId(conference.id)
+            setConferenceName(conference.name)
+          }}>
+          {conference.name}
+        </Button>
+      )}
+      <div className="mb-2">{conference.description}</div>
+      <div className="mb-2 text-sm italic">
+        {dayjs(conference.startDate).format("MMMM D, YYYY")}
+        {" - "}
+        {dayjs(conference.endDate).format("MMMM D, YYYY")}
+      </div>
+    </div>
+  )
+}
