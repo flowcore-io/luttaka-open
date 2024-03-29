ad8c404edff2febcf94870b47fafa6d2ec2ab76e
olavurellefsen <olavur@ellefsen.fo>
2024-03-29 20:53:32 +0000

    feat: show the public page, the available events or the selected event on the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,37 @@
-export default async function Home() {
-  return (
-    <main className="mx-auto w-full">
-      <div className="text-center text-slate-400">Welcome to Luttaka!</div>
-    </main>
-  )
+"use client"
+
+import { Skeleton } from "@/components/ui/skeleton"
+import { api } from "@/trpc/react"
+import { useContext } from "react"
+import { ConferenceContext } from "@/context/conference-context"
+import { useAuth } from "@clerk/nextjs"
+import HomeSelectedConference from "./home-selected-conference"
+import HomeAvailableConferences from "./home-available-conferences"
+
+export default function Home() {
+  const { isLoaded, userId } = useAuth()
+  const { conferenceId } = useContext(ConferenceContext)
+  if (!isLoaded || !userId) {
+    // Show the public page
+    return (
+      <div className="mx-auto w-full p-4 md:p-6">
+        <div className="text-center text-slate-400">Welcome to Luttaka!</div>
+      </div>
+    )
+  }
+  const { data: conferences, isLoading } = api.conference.list.useQuery()
+  if (conferences === undefined) return <Skeleton />
+  const conference = conferences?.find((c) => c.id === conferenceId)
+  if (
+    !conference ||
+    (conferenceId === null && conferences && conferences.length > 0)
+  ) {
+    return (
+      <HomeAvailableConferences
+        conferences={conferences}
+        isLoading={isLoading}
+      />
+    )
+  }
+  return <HomeSelectedConference conference={conference} />
 }
