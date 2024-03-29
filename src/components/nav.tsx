ef278da6b96ceceaffb23fb465c4856d787d4310
olavurellefsen <olavur@ellefsen.fo>
2024-03-29 20:42:24 +0000

    fix: use user.role instead of Clerk organization to establish if a user is an admin

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,14 +1,12 @@
 "use client"
 
-import { useOrganizationList } from "@clerk/nextjs"
 import type { LucideIcon } from "lucide-react"
 import type { Url } from "next/dist/shared/lib/router/router"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
-import { useEffect, useState } from "react"
-
 import { Button } from "@/components/ui/button"
 import { cn } from "@/lib/utils"
+import { api } from "@/trpc/react"
 
 interface NavProps {
   links: {
@@ -21,56 +19,40 @@ interface NavProps {
 }
 
 export function Nav({ links }: NavProps) {
+  const userRole = api.user.role.useQuery()
   const pathname = usePathname()
-  const { isLoaded, userMemberships } = useOrganizationList({
-    userMemberships: {
-      infinite: true,
-    },
-  })
-  const [superuser, setSuperuser] = useState(false)
-  useEffect(() => {
-    if (isLoaded) {
-      const isSuperuser =
-        userMemberships.data?.filter(
-          (mem) => mem.organization.slug === "flowcore",
-        ).length > 0
-      setSuperuser(!!isSuperuser)
-    }
-  }, [isLoaded, userMemberships])
 
   return (
-    isLoaded && (
-      <div className="group flex flex-col gap-4 py-4 data-[collapsed=true]:py-4">
-        <nav className="grid gap-2 group-[[data-collapsed=true]]:justify-center group-[[data-collapsed=true]]:px-2">
-          {links.map((link, index) => {
-            const isCurrent = link.href === pathname
-            if (link.superuserRequired && !superuser) {
-              return false
-            }
-            return (
-              <Button
-                key={index}
-                className="items-start justify-start"
-                asChild
-                variant={isCurrent ? "default" : "ghost"}>
-                <Link href={link.href ?? "/"}>
-                  <link.icon className="mr-2 h-5 w-5" />
-                  {link.title}
-                  {link.label && (
-                    <span
-                      className={cn(
-                        "ml-auto",
-                        isCurrent && "text-background dark:text-white",
-                      )}>
-                      {link.label}
-                    </span>
-                  )}
-                </Link>
-              </Button>
-            )
-          })}
-        </nav>
-      </div>
-    )
+    <div className="group flex flex-col gap-4 py-4 data-[collapsed=true]:py-4">
+      <nav className="grid gap-2 group-[[data-collapsed=true]]:justify-center group-[[data-collapsed=true]]:px-2">
+        {links.map((link, index) => {
+          const isCurrent = link.href === pathname
+          if (link.superuserRequired && userRole.data !== "admin") {
+            return false
+          }
+          return (
+            <Button
+              key={index}
+              className="items-start justify-start"
+              asChild
+              variant={isCurrent ? "default" : "ghost"}>
+              <Link href={link.href ?? "/"}>
+                <link.icon className="mr-2 h-5 w-5" />
+                {link.title}
+                {link.label && (
+                  <span
+                    className={cn(
+                      "ml-auto",
+                      isCurrent && "text-background dark:text-white",
+                    )}>
+                    {link.label}
+                  </span>
+                )}
+              </Link>
+            </Button>
+          )
+        })}
+      </nav>
+    </div>
   )
 }
