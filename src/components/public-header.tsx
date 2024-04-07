74c014266f918fc3c76b49ed8d7146bf4d64f0af
olavurellefsen <oe@tokni.com>
2024-04-07 13:08:40 +0100

    chore: keep Flowcore logo on public header

--- src/components/public-header.tsx
+++ src/components/public-header.tsx
@@ -1,8 +1,17 @@
 import { SignInButton } from "@clerk/nextjs"
+import Image from "next/image"
+
+import flowcoreLogo from "../../public/images/flowcore-logo.svg"
 
 const PublicHeader = () => {
   return (
-    <header className="mt-12 flex min-h-12 items-center justify-center">
+    <header className="flex min-h-12 items-center justify-between">
+      <Image
+        src={flowcoreLogo as string}
+        alt="Flowcore"
+        priority
+        className="m-8"
+      />
       <div className="hidden md:block">
         <SignInButton redirectUrl="/" mode="modal">
           <button className="dark:hover:bg-blud-700 mx-10 mb-4 h-12 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:focus:ring-blue-800 md:mb-0">
