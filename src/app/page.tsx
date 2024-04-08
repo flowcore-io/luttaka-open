21a7b4c1adf6180020b974b9de41c83920202fbe
olavurellefsen <oe@tokni.com>
2024-04-08 21:32:37 +0100

    chore: make public page black and white

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,10 +16,8 @@ export default function Home() {
   if (!isLoaded || !userId) {
     return (
       <div className="mx-auto w-full p-4 md:p-6">
-        <div className="text-center text-white">Luttaka Open Source</div>
-        <div className="text-center text-slate-400">
-          Event Experience Application
-        </div>
+        <div className="text-center">Luttaka Open Source</div>
+        <div className="text-center">Event Experience Application</div>
       </div>
     )
   }
