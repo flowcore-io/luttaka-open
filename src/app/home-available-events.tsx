7269538cb61b180b050eecf1f918b42867b8ba57
olavurellefsen <oe@tokni.com>
2024-04-08 22:13:14 +0100

    feat: show available tickets when you have not selected an event

--- src/app/home-available-events.tsx
+++ src/app/home-available-events.tsx
@@ -1,11 +1,9 @@
 import { type inferRouterOutputs } from "@trpc/server"
 
-import { PageTitle } from "@/components/ui/page-title"
 import { Skeleton } from "@/components/ui/skeleton"
 import { type appRouter } from "@/server/api/root"
-import { api } from "@/trpc/react"
 
-import { EventsList } from "../components/events-list"
+import Event from "./me/tickets/buy-ticket"
 
 type RouterOutput = inferRouterOutputs<typeof appRouter>
 
@@ -18,18 +16,14 @@ export default function HomeAvailableEvents({
   events,
   isLoading,
 }: HomeAvailableEventsProps) {
-  const apiFetchAttendingEvents = api.attendance.myEvents.useQuery()
-
   return (
     <div className="mx-auto w-full p-4 md:p-6">
-      <PageTitle
-        title={"Your events"}
-        subtitle={"A list of events that you have tickets to"}
-      />
       {isLoading || !events ? (
         <Skeleton />
       ) : (
-        <EventsList events={apiFetchAttendingEvents.data ?? []} />
+        <div>
+          {events?.map((event) => <Event key={event.id} event={event} />)}
+        </div>
       )}
     </div>
   )
