6acbf834c2816b0f6790777fdc3c8e954e42f0da
olavurellefsen <oe@tokni.com>
2024-04-11 22:29:51 +0100

    chore: reorganize imports

--- src/server/api/routers/event/event.router.ts
+++ src/server/api/routers/event/event.router.ts
@@ -1,11 +1,12 @@
 import { archiveEventProcedure } from "@/server/api/routers/event/event-archive.procedure"
 import { createEventProcedure } from "@/server/api/routers/event/event-create.procedure"
 import { getEventProcedure } from "@/server/api/routers/event/event-get.procedure"
-import { getEventPublicProcedure } from "./event-get-public.procedure"
-import { getEventPublicListProcedure } from "./event-get-public-list.procedure"
 import { getEventsProcedure } from "@/server/api/routers/event/event-list.procedure"
 import { updateEventProcedure } from "@/server/api/routers/event/event-update.procedure"
 import { createTRPCRouter } from "@/server/api/trpc"
+
+import { getEventPublicProcedure } from "./event-get-public.procedure"
+import { getEventPublicListProcedure } from "./event-get-public-list.procedure"
 
 export const eventRouter = createTRPCRouter({
   get: getEventProcedure,
