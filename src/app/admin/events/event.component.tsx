f2ac090e3a6133b419eedba4b2fec03e4d7170dc
olavurellefsen <oe@tokni.com>
2024-04-11 22:41:47 +0100

    chore: make slug optional

--- src/app/admin/events/event.component.tsx
+++ src/app/admin/events/event.component.tsx
@@ -11,7 +11,7 @@ export interface EventProps {
   event: {
     id: string
     name: string
-    slug: string
+    slug: string | null
     description: string | null
     ticketDescription: string | null
     ticketPrice: number
@@ -91,6 +91,7 @@ export function Event({ event, refetch }: EventProps) {
             <UpdateEventForm
               event={{
                 ...event,
+                slug: event.slug ?? "",
                 ticketDescription: event.ticketDescription ?? "",
                 description: event.description ?? "",
               }}
