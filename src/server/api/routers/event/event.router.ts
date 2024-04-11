95c0710410c6399b8f8c6ceb1acb0738789e4217
olavurellefsen <oe@tokni.com>
2024-04-11 22:27:49 +0100

    chore: add getPublicList method to the API

--- src/server/api/routers/event/event.router.ts
+++ src/server/api/routers/event/event.router.ts
@@ -2,6 +2,7 @@ import { archiveEventProcedure } from "@/server/api/routers/event/event-archive.
 import { createEventProcedure } from "@/server/api/routers/event/event-create.procedure"
 import { getEventProcedure } from "@/server/api/routers/event/event-get.procedure"
 import { getEventPublicProcedure } from "./event-get-public.procedure"
+import { getEventPublicListProcedure } from "./event-get-public-list.procedure"
 import { getEventsProcedure } from "@/server/api/routers/event/event-list.procedure"
 import { updateEventProcedure } from "@/server/api/routers/event/event-update.procedure"
 import { createTRPCRouter } from "@/server/api/trpc"
@@ -9,6 +10,7 @@ import { createTRPCRouter } from "@/server/api/trpc"
 export const eventRouter = createTRPCRouter({
   get: getEventProcedure,
   getPublic: getEventPublicProcedure,
+  getPublicList: getEventPublicListProcedure,
   list: getEventsProcedure,
   create: createEventProcedure,
   update: updateEventProcedure,
