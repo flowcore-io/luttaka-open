1dee8086ef09615bc71c790e9c4e664b5cd7940f
olavurellefsen <oe@tokni.com>
2024-04-11 22:43:44 +0100

    chore: restructuring of the public and protected root pages

--- src/app/home-available-events.tsx
+++ src/app/home-available-events.tsx
@@ -3,7 +3,6 @@ import { type inferRouterOutputs } from "@trpc/server"
 import { PageTitle } from "@/components/ui/page-title"
 import { Skeleton } from "@/components/ui/skeleton"
 import { type appRouter } from "@/server/api/root"
-import { api } from "@/trpc/react"
 
 import { EventsList } from "../components/events-list"
 
@@ -18,18 +17,13 @@ export default function HomeAvailableEvents({
   events,
   isLoading,
 }: HomeAvailableEventsProps) {
-  const apiFetchAttendingEvents = api.attendance.myEvents.useQuery()
-
   return (
     <div className="mx-auto w-full p-4 md:p-6">
-      <PageTitle
-        title={"Your events"}
-        subtitle={"A list of events that you have tickets to"}
-      />
+      <PageTitle title={"Available events"} />
       {isLoading || !events ? (
         <Skeleton />
       ) : (
-        <EventsList events={apiFetchAttendingEvents.data ?? []} />
+        <EventsList events={events ?? []} />
       )}
     </div>
   )
