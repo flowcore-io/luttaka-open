fa90fa9b7135ade35fc62ef0df25229eee82cf5b
olavurellefsen <oe@tokni.com>
2024-04-12 22:14:09 +0100

    chore: sort imports

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -4,9 +4,10 @@ import { eventRouter } from "@/server/api/routers/event/event.router"
 import { profileRouter } from "@/server/api/routers/profile/profile.router"
 import { ticketRouter } from "@/server/api/routers/ticket/ticket.router"
 import { userRouter } from "@/server/api/routers/user/user.router"
-import { newsitemRouter } from "./routers/newsitem/newsitem.router"
-import { activityRouter } from "./routers/activity/activity.router"
 import { createTRPCRouter } from "@/server/api/trpc"
+
+import { activityRouter } from "./routers/activity/activity.router"
+import { newsitemRouter } from "./routers/newsitem/newsitem.router"
 
 /**
  * This is the primary router for your server.
