ab6c82942879d90fcf03658552d28672d6c0b279
flowcore-platform <gitops@flowcore.io>
2024-04-20 20:46:16 +0100

    optimised history query by theoretically 80%

--- src/server/api/routers/ticket/ticket-transferred.procedure.ts
+++ src/server/api/routers/ticket/ticket-transferred.procedure.ts
@@ -1,4 +1,4 @@
-import { and, asc, eq, gte } from "drizzle-orm"
+import { and, asc, eq, sql } from "drizzle-orm"
 
 import { db } from "@/database"
 import {
@@ -20,64 +20,96 @@ export type TicketTransferHistory = {
   id: string
   event: string
   recipients: TicketTransferHistoryRecipient[]
-  ticketNote: string
 }
+
+type TicketHistoryRow = {
+  ticketId: string
+  event: string | null
+  transferTimestamp: number
+  recipientEmail: string | null
+  recipientFirstName: string | null
+  recipientLastName: string | null
+}[]
 
 export const ticketsTransferredProcedure = protectedProcedure.query<
   TicketTransferHistory[]
 >(async ({ ctx }): Promise<TicketTransferHistory[]> => {
   const userId = ctx.user.id
 
-  // todo: this entire thing should be a single query
-  const originPoints = await db
-    .select({
-      timestamp: ticketOwnershipHistory.timestamp,
-      ticketId: ticketOwnershipHistory.ticketId,
-      events: events.name,
-    })
-    .from(ticketOwnershipHistory)
-    .where(eq(ticketOwnershipHistory.userId, userId))
-    .leftJoin(tickets, eq(tickets.id, ticketOwnershipHistory.ticketId))
-    .leftJoin(events, eq(events.id, tickets.eventId))
-    .execute()
-
-  const result = await Promise.all(
-    originPoints.map(async (originPoint): Promise<TicketTransferHistory> => {
-      const ticketChanges = await db
-        .select({
-          timestamp: ticketOwnershipHistory.timestamp,
-          ticketId: ticketOwnershipHistory.ticketId,
-          userId: ticketOwnershipHistory.userId,
-          recipientEmail: profiles.emails,
-          recipientName: profiles.firstName,
-          recipientLastName: profiles.lastName,
-        })
-        .from(ticketOwnershipHistory)
-        .where(
-          and(
-            eq(ticketOwnershipHistory.ticketId, originPoint.ticketId),
-            gte(ticketOwnershipHistory.timestamp, originPoint.timestamp),
-          ),
-        )
-        .leftJoin(profiles, eq(profiles.userId, ticketOwnershipHistory.userId))
-        .orderBy(asc(ticketOwnershipHistory.timestamp))
-        .execute()
-
-      return {
-        id: originPoint.ticketId,
-        event: originPoint.events ?? "",
-        recipients: ticketChanges.map(
-          (ticketChange): TicketTransferHistoryRecipient => ({
-            email: ticketChange.recipientEmail ?? "",
-            firstName: ticketChange.recipientName ?? "",
-            lastName: ticketChange.recipientLastName ?? "",
-            timestamp: ticketChange.timestamp,
-          }),
+  // Using a CTE to find the earliest moment the user owned a ticket
+  // The reason being is that want to show all the transfers that happened after the user owned the ticket
+  // (Meaning you can't see transfers that happened before the user owned the ticket)
+  const earliestMomentUserOwnedTicket = db.$with("earliestTimestamp").as(
+    db
+      .select({
+        ticketId: ticketOwnershipHistory.ticketId,
+        earliestTimestamp: sql`min(${ticketOwnershipHistory.timestamp})`.as(
+          "earliestTimestamp",
         ),
-        ticketNote: "",
-      }
-    }),
+      })
+      .from(ticketOwnershipHistory)
+      .where(eq(ticketOwnershipHistory.userId, userId))
+      .groupBy(ticketOwnershipHistory.ticketId),
   )
 
-  return result
+  const result = await db
+    .with(earliestMomentUserOwnedTicket)
+    .select({
+      ticketId: ticketOwnershipHistory.ticketId,
+      event: events.name,
+      transferTimestamp: ticketOwnershipHistory.timestamp,
+      recipientEmail: profiles.emails,
+      recipientFirstName: profiles.firstName,
+      recipientLastName: profiles.lastName,
+    })
+    .from(ticketOwnershipHistory)
+    .leftJoin(tickets, eq(tickets.id, ticketOwnershipHistory.ticketId))
+    .leftJoin(events, eq(events.id, tickets.eventId))
+    .leftJoin(profiles, eq(profiles.userId, ticketOwnershipHistory.userId))
+    .leftJoin(
+      earliestMomentUserOwnedTicket,
+      eq(
+        earliestMomentUserOwnedTicket.ticketId,
+        ticketOwnershipHistory.ticketId,
+      ),
+    )
+    .where(
+      and(
+        sql`${ticketOwnershipHistory.timestamp} >= ${earliestMomentUserOwnedTicket.earliestTimestamp}`,
+      ),
+    )
+    .orderBy(asc(ticketOwnershipHistory.timestamp))
+
+  const collection: Record<string, TicketTransferHistory> =
+    groupRecipientsByTicketId(result)
+
+  // todo: create a second CTE to filter out all tickets that have the current user id and a single ticket ownerhsip.
+  return Object.values(collection).filter(
+    (ticket) => ticket.recipients.length > 1,
+  )
 })
+
+function groupRecipientsByTicketId(result: TicketHistoryRow) {
+  const collection: Record<string, TicketTransferHistory> = {}
+  for (const row of result) {
+    if (!row.ticketId) {
+      continue
+    }
+
+    if (collection[row.ticketId] === undefined) {
+      collection[row.ticketId] = {
+        id: row.ticketId,
+        event: row.event ?? "",
+        recipients: [],
+      }
+    }
+
+    collection[row.ticketId]?.recipients.push({
+      email: row.recipientEmail ?? "",
+      firstName: row.recipientFirstName ?? "",
+      lastName: row.recipientLastName ?? "",
+      timestamp: row.transferTimestamp,
+    })
+  }
+  return collection
+}
